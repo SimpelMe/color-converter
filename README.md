06d2b7d1090ed72e27e5ece3399890e885335f9e
Conrad Zelck <git@simpel.cc>
2021-06-13 22:26:21 +0000

    initial commit
    Signed-off-by: Conrad Zelck <git@simpel.cc>

---
+++ README.md
@@ -0,0 +1,17 @@
+# Color Converter
+It's a simple web color converter that can take multiple color formats as input and converts them into different color formats in real-time.
+
+It works with:
+* Hex
+* RGB
+* HSL
+* HSV
+* Float
+* Swift
+* Objective-C
+* Xamarin (C#)
+* Name (e.g. grey)
+
+## Dependencies:
+* [jQuery](https://github.com/jquery/jquery) - Making life bearable
+* [TinyColor](https://github.com/bgrins/TinyColor) - Old color converter. No longer maintained
---
+++ js/copy.js
@@ -0,0 +1,44 @@
+function copyhex() {
+  var text = document.getElementById("hex-output").innerHTML;
+  navigator.clipboard.writeText(text);
+}
+
+function copyhex8() {
+  var text = document.getElementById("hex8-output").innerHTML;
+  navigator.clipboard.writeText(text);
+}
+
+function copyrgb() {
+  var text = document.getElementById("rgb-output").innerHTML;
+  navigator.clipboard.writeText(text);
+}
+
+function copyswift() {
+  var text = document.getElementById("swift-output").innerHTML;
+  navigator.clipboard.writeText(text);
+}
+
+function copyobjectivec() {
+  var text = document.getElementById("objective-c-output").innerHTML;
+  navigator.clipboard.writeText(text);
+}
+
+function copyxamarin() {
+  var text = document.getElementById("xamarin-output").innerHTML;
+  navigator.clipboard.writeText(text);
+}
+
+function copyfloat() {
+  var text = document.getElementById("float-output").innerHTML;
+  navigator.clipboard.writeText(text);
+}
+
+function copyhsl() {
+  var text = document.getElementById("hsl-output").innerHTML;
+  navigator.clipboard.writeText(text);
+}
+
+function copyhsv() {
+  var text = document.getElementById("hsv-output").innerHTML;
+  navigator.clipboard.writeText(text);
+}
